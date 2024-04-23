1e491fd079f5a999001a445be0f42085842614e7
Ibewunjo Enoch <fredenoch1@gmail.com>
2024-04-23 04:27:28 -0700

    deleted old migration

--- prisma/migrations/20240418080833_updatedrating/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `rating` column on the `product` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE `product` DROP COLUMN `rating`,
-    ADD COLUMN `rating` INTEGER NULL;
