eeedd24fcb1f2f1a971184f51e84b5278cc6c8a1
Ibewunjo Enoch <fredenoch1@gmail.com>
2024-04-24 09:28:22 -0700

    add pagination

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -178,7 +178,6 @@ export const deleteProduct = async (req: Request, res: Response): Promise<void> 
 export const getAllProduct = async (req: Request, res: Response): Promise<void> => {
   try {
     const product = await prisma.product.findMany({
-      take: 15,
       include: {
         brand: true,
         category: true,
