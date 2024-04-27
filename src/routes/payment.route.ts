ade2c0ad59ba0237d8489e408bd1599477681abe
Ibewunjo Enoch <fredenoch1@gmail.com>
2024-04-27 01:36:01 -0700

    change route and added authetication

--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -1,11 +1,11 @@
 import express, {Request, Response} from 'express'
 import { deletetransaction, getAlltransactionForAuser, getAlltransactions, getAtransactionOfAuser, makePayment, paginatePayment } from '../controller/payment.controller';
-import { protectAdmin } from '../middleware/auth.middleware';
+import { protectAdmin, protect } from '../middleware/auth.middleware';
 const paymentRoute = express.Router();
 
 paymentRoute.post('/checkout/transaction', makePayment );
-paymentRoute.get('/transactions/:id', getAlltransactionForAuser );
-paymentRoute.get('/transaction/:userId/:id', getAtransactionOfAuser );
+paymentRoute.get('/transactions/:id', protect, getAlltransactionForAuser );
+paymentRoute.get('/transaction/:userId/:id', protect, getAtransactionOfAuser );
 paymentRoute.get('/transactions', protectAdmin, getAlltransactions );
 paymentRoute.delete('/transaction/:id', protectAdmin, deletetransaction );
 paymentRoute.post('/transaction/paginate', protectAdmin, paginatePayment );
